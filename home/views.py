6dde1755541575397461edc8e43c18db36db7037
wowsh <tkdtkdghkt@gmail.com>
2017-05-10 15:25:30 +0000

    correct view render path

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     msg = "My message"
-    return render(request, 'index.html', {'message':msg})
+    return render(request, 'home/index.html', {'message':msg})
