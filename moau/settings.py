49ee000e7a8943180f452444921f582b4da42437
JuKook <wnrnrdl@google.com>
2017-05-17 12:59:46 +0000

    홈 url 추가

--- moau/settings.py
+++ moau/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'mypage',
+    'bookmarket',
+    'groupbuying',
+    'taxipool',
 ]
 
 MIDDLEWARE_CLASSES = [
