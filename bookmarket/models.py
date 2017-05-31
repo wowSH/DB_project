bff874e5c86144d5dcc296e02767e806f63c7970
wowsh <tkdtkdghkt@gmail.com>
2017-05-31 17:00:28 +0000

    add bookmarket models.py

---
+++ bookmarket/models.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Sel_Reg (models.Model):
+    s_name = models.CharField(max_length = 40)
+    s_hp = models.CharField(max_length = 40)
+    
+    register_date = models.DateTimeField()
+    init_price = models.IntegerField()
+    imm_price = models.IntegerField()
+    closing_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.s_name
+
+class Product(models.Model):
+    title = models.CharField(max_length = 40)
+    category = models.CharField(max_length = 40)
+    author = models.CharField(max_length = 40)
+    subject = models.CharField(max_length = 40)
+    state = models.CharField(max_length = 5)
+    condition = models.CharField(max_length = 5)
+    
+    sel_reg = models.ForeignKey(Sel_Reg, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title +"/"+ self.state
+
+class Candidate(models.Model):
+    c_name = models.CharField(max_length = 40)
+    c_hp = models.IntegerField(default=0)
+   
+    bid = models.ManyToManyField(Product, through='Bid')
+    
+    def __str__(self):
+        return self.c_name
+
+
+class Bid(models.Model):
+    bid_date = models.DateTimeField()
+    bid_price = models.IntegerField()
+    priority = models.IntegerField()
+   
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    
