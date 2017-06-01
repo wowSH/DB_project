b753fe0ced9f00a1e921d57a56a2176117756b2c
youwon35 <youwon35@naver.com>
2017-06-01 23:43:25 +0900

    Add files via upload

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+from accounts import views
+
+urlpatterns = patterns('',
+    url(
+        r'^logout/$',
+        'django.contrib.auth.views.logout',
+        name='logout',
+        kwargs={'next_page': '/'}
+    ),
+)
---
+++ groupbuying/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+###########################################################
+def index (request):
+    return HttpResponse("Groupbuying")
+
+###########################################################
+def open(request):
+    return
+###########################################################
+
+
+###########################################################
+def apply(request):
+    return
+###########################################################
