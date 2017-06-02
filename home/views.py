b41544fabf5809ab64d701650832039c688fc929
JuKook <wnrnrdl@google.com>
2017-06-02 14:18:44 +0000

    영어 홈도 수정 완료

--- home/views.py
+++ home/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 
-    return render(request, 'home/index.html')
-    # return render(request, 'home/index_eng.html')
+    # return render(request, 'home/index.html')
+    return render(request, 'home/index_eng.html')
