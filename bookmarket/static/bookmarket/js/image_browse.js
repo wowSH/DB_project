1cc0893ce5c34d26f7fa862c905b9dda0d1958bc
wowsh <tkdtkdghkt@gmail.com>
2017-06-02 17:28:10 +0000

    add register modal

---
+++ bookmarket/static/bookmarket/js/image_browse.js
@@ -0,0 +1,8 @@
+$(document).on('ready', function() {
+    $("#input-20").fileinput({
+        browseClass: "btn btn-primary btn-block",
+        showCaption: false,
+        showRemove: false,
+        showUpload: false
+    });
+});
